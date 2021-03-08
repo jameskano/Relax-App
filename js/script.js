4e3cd605757950d3321e29faf809d7ccc349efbb
jameskano <jaimecanovasdesign@gmail.com>
2021-03-08 18:38:30 +0100

    App completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
 // jshint esversion:9
+
+const text = document.querySelector(".container p");
+const container = document.querySelector(".container");
+
+
+function textTemp() {
+  container.classList.remove("contract");
+  container.classList.add("expand");
+  text.innerText = "Breathe In";
+
+  setTimeout( function() {
+    text.innerText = "Hold...";
+
+    setTimeout(function() {
+      container.classList.remove("expand");
+      container.classList.add("contract");
+      text.innerText = "Breathe Out";
+
+      setTimeout(function() {
+        text.innerText = "Hold...";
+      }, 4000);
+    }, 2000);
+  }, 4000);
+}
+
+textTemp();
+let interval = setInterval(textTemp, 12000);
